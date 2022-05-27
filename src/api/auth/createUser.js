654abd4f85d85bba34b67cb153bf0ba4e701ffd3
Donnn2 <kdowon92@naver.com>
2022-05-27 23:19:28 +0900

    SignUp

--- src/api/auth/createUser.js
+++ src/api/auth/createUser.js
@@ -1,4 +1,24 @@
-import { gql } from "graphql-request";
+import request, { gql } from "graphql-request";
 
-export const createUserMutation = gql``;
-export const createUserRequest = async () => {}
+export const hyEndpoint = "http://hychurch.duckdns.org:3000/graphql";
+
+export const createUserMutation = gql`
+  mutation singUp($name:String!,$phone:String!,$password:String!,$authenticationNumber:String!){
+    signUp(input:{
+      name: $name,
+      phone: $phone,
+      password: $password,
+      authenticationNumber: $authenticationNumber
+    }){
+      user{
+        id
+        name
+        phone
+      }
+  }
+}
+`;
+
+export const createUserRequest = async (name,phone,password,passwordConfirm,authenticationNumber) => {
+  return await request(hyEndpoint,createUserMutation,{name,phone,password,passwordConfirm,authenticationNumber})
+}
